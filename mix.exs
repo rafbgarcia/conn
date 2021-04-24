eb2f804dd33fac5bae986ca01cfa41e157d1716d
Rafael Garcia <rafbgarcia@gmail.com>
2021-04-24 15:11:32 -0300

    Add elixir_uuid for timeuuid
    NoSQL DBs don't return the inserted value. Hence, we need to know its ID to get it back after an insert.

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Connect.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:xandra, "~> 0.13"}
+      {:xandra, "~> 0.13"},
+      {:elixir_uuid, "~> 1.2"}
     ]
   end
 
