0b4022ca7656aab65ca46315b25016a4b3cb86a2
Rafael Garcia <rafbgarcia@gmail.com>
2021-04-24 11:27:44 -0300

    Add xandra as the Scylla driver

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule Connect.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:xandra, "~> 0.13"}
     ]
   end
 
