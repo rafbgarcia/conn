55e784de5e9e49e5bdb0f346b0b8144e8af5918f
Rafael Garcia <rafbgarcia@gmail.com>
2021-05-01 15:15:47 -0300

    Make documents easier to read

---
+++ server/docs/Test Coverage.md
@@ -0,0 +1,22 @@
+Percentage | Module
+-----------|--------------------------
+[31m     0.00%[0m | Db.Server[0m
+[31m    20.00%[0m | Db.Repo[0m
+[31m    50.00%[0m | ConnectWeb.GraphqlContext[0m
+[31m    66.67%[0m | Db.ThreadMessage[0m
+[31m    72.73%[0m | Db.Snowflake[0m
+[31m    83.33%[0m | Db.ChannelMember[0m
+[32m   100.00%[0m | Connect[0m
+[32m   100.00%[0m | ConnectWeb.Guardian[0m
+[32m   100.00%[0m | ConnectWeb.Resolvers.Auth[0m
+[32m   100.00%[0m | ConnectWeb.Resolvers.Badges[0m
+[32m   100.00%[0m | ConnectWeb.Resolvers.ChannelMembers[0m
+[32m   100.00%[0m | ConnectWeb.Resolvers.Channels[0m
+[32m   100.00%[0m | ConnectWeb.Resolvers.Messages[0m
+[32m   100.00%[0m | ConnectWeb.UserSocket[0m
+[32m   100.00%[0m | Db.Account[0m
+[32m   100.00%[0m | Db.Channel[0m
+[32m   100.00%[0m | Db.Message[0m
+[32m   100.00%[0m | Db.User[0m
+-----------|--------------------------
+[31m    85.61%[0m | Total[0m
--- server/lib/mix/tasks/connect.test_doc.ex
+++ server/lib/mix/tasks/connect.test_doc.ex
@@ -7,17 +7,40 @@ defmodule Mix.Tasks.Connect.TestsDoc do
   def run(_args) do
     Mix.Task.run("app.start")
 
-    result =
-      capture_io(fn ->
-        Mix.Task.run("test", [
-          "--trace",
-          "--cover",
-          "test/graphql"
-        ])
-      end)
+    capture_io(fn ->
+      Mix.Task.run("test", [
+        "--trace",
+        "test/graphql"
+      ])
+    end)
+    |> sanitize_tests()
+    |> save_to_file("GraphQL Queries Test Cases")
 
-    {:ok, file} = File.open("./tests_doc", [:write])
-    IO.binwrite(file, result)
+    capture_io(fn -> Mix.Task.run("test.coverage") end)
+    |> sanitize_coverage()
+    |> save_to_file("Test Coverage")
+  end
+
+  defp sanitize_tests(text) do
+    text
+    |> String.split("\n")
+    |> Enum.slice(1..-7)
+    |> Enum.join("\n")
+    |> String.replace(~r/[\[\(][^\n]*/, "")
+    |> String.replace("Connect.Graphql.", "### ")
+    |> String.replace(~r/test /i, "")
+  end
+
+  defp sanitize_coverage(text) do
+    text
+    |> String.split("\n")
+    |> Enum.slice(2..-3)
+    |> Enum.join("\n")
+  end
+
+  defp save_to_file(output, name) do
+    {:ok, file} = File.open("./docs/#{name}.md", [:write])
+    IO.binwrite(file, output)
     File.close(file)
   end
 end
