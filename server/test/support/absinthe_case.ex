efa0686f4587c748d5f8512da9bd971efe5b43d0
Rafael Garcia <rafbgarcia@gmail.com>
2021-05-01 11:01:06 -0300

    Create assert_errors_equals assertion function

--- server/test/support/absinthe_case.ex
+++ server/test/support/absinthe_case.ex
@@ -7,8 +7,25 @@ defmodule ConnectWeb.AbsintheCase do
       import Connect.Factory
 
       def run(document, opts \\ []) do
-        {:ok, %{data: response}} = Absinthe.run(document, ConnectWeb.Schema, opts)
-        response
+        {:ok, %{data: data}} = Absinthe.run(document, ConnectWeb.Schema, opts)
+        data
+      end
+
+      def assert_errors_equals(document, expected, options \\ [])
+
+      def assert_errors_equals(document, expected, options) when is_list(expected) do
+        ExUnit.Assertions.assert(error_messages(document, options) == expected)
+      end
+
+      def assert_errors_equals(document, expected, options) when is_binary(expected) do
+        messages = error_messages(document, options)
+        ExUnit.Assertions.assert(length(messages) == 1)
+        ExUnit.Assertions.assert(messages == [expected])
+      end
+
+      defp error_messages(document, options) do
+        {:ok, %{errors: errors}} = Absinthe.run(document, ConnectWeb.Schema, options)
+        error_messages = Enum.map(errors, & &1.message)
       end
     end
   end
