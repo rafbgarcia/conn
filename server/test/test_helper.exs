8d04de34bbb35468863806bbf632f91945fc2d24
Rafael Garcia <rafbgarcia@gmail.com>
2021-05-01 08:39:44 -0300

    Don't run parallel tests

--- server/test/test_helper.exs
+++ server/test/test_helper.exs
@@ -1,2 +1,9 @@
 Connect.FactorySequence.start_link([])
-ExUnit.start()
+
+# Why max_cases 1?
+# Common Phoenix and Ecto projects run tests in a Sandbox,
+# hence they don't affect each other and can run in parallel.
+#
+# There is not tool available at this moment that does the
+# same for Cassandra/ScyllaDB.
+ExUnit.start(max_cases: 1)
